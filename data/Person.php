1136121574afc00b924b44a632f811367f01ddb9
Ahmad Syarifuddin <oden.psht@gmail.com>
2025-09-05 17:18:56 +0700

    Function

--- data/Person.php
+++ data/Person.php
@@ -4,4 +4,8 @@ class Person{
     var string $name;
     var ?string $address;
     var string $country = "Indonesia";
+
+    function sayHello(string $name) {
+        echo "Hello $name" . PHP_EOL;
+    }
 }
