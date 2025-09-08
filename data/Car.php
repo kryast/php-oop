37380a86005d36422b2adc92a8f58ffa8dcfb57e
Ahmad Syarifuddin <oden.psht@gmail.com>
2025-09-08 20:11:25 +0700

    Interface

---
+++ data/Car.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Data;
+
+interface Car {
+
+    function drive(): void;
+
+    function getTire(): int;
+}
+
+class Avanza implements Car{
+
+    function drive(): void
+    {
+        echo "Drive Avanza" .PHP_EOL;
+    }
+
+    function getTire(): int
+    {
+        return 4;
+    }
+}
