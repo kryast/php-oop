62bb3881b6658479becf758d0c0c91a2f3937f32
Ahmad Syarifuddin <oden.psht@gmail.com>
2025-09-09 20:43:29 +0700

    Static Function

--- helper/MathHelper.php
+++ helper/MathHelper.php
@@ -5,4 +5,14 @@ namespace helper;
 class MathHelper {
 
     static public string $name = "MathHelper";
+
+    static public function sum(int... $numbers): int {
+
+        $total = 0;
+
+        foreach($numbers as $number) {
+            $total += $number;
+        }
+        return $total;
+    }
 }
--- static.php
+++ static.php
@@ -11,3 +11,6 @@ echo MathHelper::$name . PHP_EOL;
 MathHelper::$name = "Ahmad Syarifuddin";
 
 echo MathHelper::$name . PHP_EOL;
+
+$result = MathHelper::sum(10,10,10,10,10);
+echo "Result : $result" . PHP_EOL;
