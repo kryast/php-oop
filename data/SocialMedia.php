591e80154c4aed7b21714e6adbaa110aa313207e
Ahmad Syarifuddin <oden.psht@gmail.com>
2025-09-09 20:26:20 +0700

    Final Function

--- data/SocialMedia.php
+++ data/SocialMedia.php
@@ -4,8 +4,16 @@ class SocialMedia {
     public string $name;
 }
 
-final class Facebook extends SocialMedia{
+class Facebook extends SocialMedia{
 
+    final public function login(string $username, string $password): bool {
+        return true;
+    }
 }
 
-// class FakeFacebook extends Facebook{} Error 
+class FakeFacebook extends Facebook{
+
+    // public function login(string $username, string $password): bool {
+    //     return true;
+    // } Error
+}
